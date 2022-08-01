596a5b73ac2abb9622b182fe0390d8e854a608c8
Trong Doan <doantrong8777@gmail.com>
2022-08-01 09:44:42 -0700

    barista

--- Orders_Items/TestOrders.java
+++ Orders_Items/TestOrders.java
@@ -3,54 +3,32 @@ public class TestOrders {
     public static void main(String[] args) {
     
         // Menu items
-        Item item1 = new Item();
-        item1.name = "mocha";
-        item1.price = 3.25;
-        Item item2 = new Item();
-        item2.name = "latte";
-        item2.price = 4.5;
-        Item item3 = new Item();
-        item3.name = "drip coffee";
-        item3.price = 2.15;
-        Item item4 = new Item();
-        item4.name = "capuccino";
-        item4.price = 3.5;
+        Item item1 = new Item("drip coffee", 1.5);
+        Item item2 = new Item("cappuccino",3.5);
+        Item item3 = new Item("esspresso", 1.0);
 
         // Order variables -- order1, order2 etc.
-        Order order1 = new Order();
-        order1.name = "Cindhuri";
-        Order order2 = new Order();
-        order2.name = "Jimmy";
-        Order order3 = new Order();
-        order3.name = "Noah";
-        Order order4 = new Order();
-        order4.name = "Sam";
+        Order order1 = new Order("Trong");
+        Order order2 = new Order("Ethan");
+        Order order3 = new Order("Doan");
 
         // Application Simulations
-        order2.items.add(item1);
-        order2.total += item1.price;
-        order3.items.add(item4);
-        order3.total += item4.price;
-        order4.items.add(item2);
-        order4.total += item2.price;
-        order1.ready = true;
-        order4.items.add(item2);
-        order4.items.add(item2);
-        order4.total += item2.price * 2;
-        order2.ready = true;
+        order1.additem(item1);
+        order1.additem(item2);
+        order2.additem(item1);
+        order2.additem(item3);
+        order3.additem(item2);
+        order3.additem(item3);
 
+        order2.setReady(true);
+        System.out.println(order2.getStatusMessage());
+
+        System.out.println(order1.getOrderTotal());
+
+        order1.display();
+        order2.display();
+        order3.display();
         // Use this example code to test various orders' updates
-        System.out.printf("Name: %s\n", order1.name);
-        System.out.printf("Total: %s\n", order1.total);
-        System.out.printf("Ready: %s\n", order1.ready);
-        System.out.printf("Name: %s\n", order2.name);
-        System.out.printf("Total: %s\n", order2.total);
-        System.out.printf("Ready: %s\n", order2.ready);
-        System.out.printf("Name: %s\n", order3.name);
-        System.out.printf("Total: %s\n", order3.total);
-        System.out.printf("Ready: %s\n", order3.ready);
-        System.out.printf("Name: %s\n", order4.name);
-        System.out.printf("Total: %s\n", order4.total);
-        System.out.printf("Ready: %s\n", order4.ready);
+       
     }
 }
