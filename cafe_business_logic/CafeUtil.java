1ee10eafe838f5cb17eeaa2b3c25edba3eab27f0
Trong Doan <doantrong8777@gmail.com>
2022-07-31 21:54:11 -0700

    CafeUtil

--- cafe_business_logic/CafeUtil.java
+++ cafe_business_logic/CafeUtil.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class CafeUtil{
     public int getStreakGoal(int numWeeks){
         int result = 0;
@@ -5,5 +6,28 @@ public class CafeUtil{
             result += i;
         }
         return result;
+    }
+
+    public double getOrderTotal(double[] prices){
+        double total = 0;
+        for(int i = 0; i < prices.length; i++){
+            total += prices[i];
+        }
+        return total;
+    }
+
+    public void displayMenu(ArrayList<String> menuItems){
+        for(int i = 0; i < menuItems.size(); i++){
+            System.out.println(i + " " + menuItems.get(i));
+        }
+    }
+
+    public void addCustomer(ArrayList<String>customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.println("Hello " + userName );
+        System.out.println("There are " + customers.size() + " ahead of you");
+        customers.add(userName);
+        System.out.println(customers);
     }
 }
